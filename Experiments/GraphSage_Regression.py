eb4b5944a83bbae3d96261b056d1369847b727fc
intello72 <mihiragarwal1710@gmail.com>
2023-05-18 22:40:46 +0530

    Added Docstrings

--- Experiments/GraphSage_Regression.py
+++ Experiments/GraphSage_Regression.py
@@ -1,16 +1,37 @@
+import torch
 import torch.nn as nn
 import torch.nn.functional as F
 from torch_geometric.nn import GraphSAGEConv
 
+
 class RegressionModel(nn.Module):
+    """
+    A simple regression model using a GraphSAGE.
+
+    Args:
+        input_dim (int): The dimensionality of the input features.
+
+    """
+
     def __init__(self, input_dim):
-        super(RegressionModel, self).__init__()
+        super().__init__()
         self.conv1 = GraphSAGEConv(input_dim, 16)
         self.conv2 = GraphSAGEConv(16, 10)
         self.linear1 = nn.Linear(10, 10)
         self.linear2 = nn.Linear(10, 1)
 
     def forward(self, x, edge_index):
+        """
+        Forward pass.
+
+        Args:
+            x (torch.Tensor): The input features.
+            edge_index (torch.Tensor): The edge indices.
+
+        Returns:
+            torch.Tensor: The output predictions.
+
+        """
         x = self.conv1(x, edge_index)
         x = F.relu(x)
         x = self.conv2(x, edge_index)
@@ -18,3 +39,4 @@ class RegressionModel(nn.Module):
         x = F.relu(x)
         x = self.linear2(x)
         return x
+
