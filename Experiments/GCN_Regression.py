310505311841e7db62954ce8caa96d1fcb36cbd2
intello72 <mihiragarwal1710@gmail.com>
2023-05-18 22:38:27 +0530

    Regression Function Files

---
+++ Experiments/GCN_Regression.py
@@ -0,0 +1,20 @@
+import torch.nn as nn
+import torch.nn.functional as F
+from torch_geometric.nn import GCNConv
+
+class RegressionModel(nn.Module):
+    def __init__(self, input_dim):
+        super(RegressionModel, self).__init__()
+        self.conv1 = GCNConv(input_dim, 16)
+        self.conv2 = GCNConv(16, 10)
+        self.linear1 = nn.Linear(10, 10)
+        self.linear2 = nn.Linear(10, 1)
+
+    def forward(self, x, edge_index):
+        x = self.conv1(x, edge_index)
+        x = F.relu(x)
+        x = self.conv2(x, edge_index)
+        x = self.linear1(x)
+        x = F.relu(x)
+        x = self.linear2(x)
+        return x
